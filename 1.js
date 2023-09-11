4222fca99d3a46940cd67ca769d0f95e55212bca
Your Name <you@example.com>
2023-09-11 23:36:27 +0400

    javaScriptHW3_1_2

--- 1.js
+++ 1.js
@@ -6,8 +6,8 @@
 Необходимо продемонстрировать вызов данной функции, с выводом результата, 
 который получаем от функции, в консоль.
 */
-const cubNumber = (number) => {
-  return number ** 3;
-};
-const inputNumber = Number(prompt("Введите число"));
-console.log(cubNumber(inputNumber));
+// const cubNumber = (number) => {
+//   return number ** 3;
+// };
+// const inputNumber = Number(prompt("Введите число"));
+// console.log(cubNumber(inputNumber));
