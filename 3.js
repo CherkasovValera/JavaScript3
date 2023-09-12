d3e965cf442f29e7a86d5c449e91130bfd3be553
Your Name <you@example.com>
2023-09-12 22:57:37 +0400

    javaScriptHW3_1_2_3_4final

--- 3.js
+++ 3.js
@@ -9,11 +9,10 @@
 Примечание: Условимся, что пользователь всегда вводит корректные значения, 
 три числа. Проверять их не нужно.
 */
-// function maxNambers(num1, num2, num3) {
-//     console.log(`"Максимальное значение среди чисел ${num1}, ${num2}, ${num3} равно ${Math.max(num1, num2, num3)}"`);
-//     return;
-// }
-// const inputNum1 = Number(prompt("Введите число №1"));
-// const inputNum2 = Number(prompt("Введите число №2"));
-// const inputNum3 = Number(prompt("Введите число №3"));
-// maxNambers(inputNum1, inputNum2, inputNum3);
+function maxNambers(num1, num2, num3) {
+    console.log(`"Максимальное значение среди чисел ${num1}, ${num2}, ${num3} равно ${Math.max(num1, num2, num3)}"`);
+}
+const inputNum1 = Number(prompt("Введите число №1"));
+const inputNum2 = Number(prompt("Введите число №2"));
+const inputNum3 = Number(prompt("Введите число №3"));
+maxNambers(inputNum1, inputNum2, inputNum3);
